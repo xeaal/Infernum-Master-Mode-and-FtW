a96806459f8bc8053cadac6e3199cf7a93b0e6ac
xeaal <tryax.use@gmail.com>
2023-01-14 15:59:56 +0500

    Updated mod to current version of Infernum

--- InfernumMasterFtW.cs
+++ InfernumMasterFtW.cs
@@ -2,35 +2,17 @@ using System;
 using System.Reflection;
 using Terraria;
 using Terraria.ModLoader;
-using MonoMod.RuntimeDetour.HookGen;
-using MonoMod.Cil;
-using Mono;
 
 namespace InfernumMasterFtW
 {
 	public class InfernumMasterFtW : Mod {
 		public override void Load() {
 			if (!ModLoader.HasMod("InfernumMode")) return;
-            HookEndpointManager.Modify(typeof(InfernumMode.PoDPlayer).GetMethod("PostUpdateMiscEffects", BindingFlags.Public | BindingFlags.Instance), IL_PostUpdateMiscEffects);
+			InfernumMode.Core.GlobalInstances.Systems.DifficultyManagementSystem.DisableDifficultyModes = false;
         }
-		private void IL_PostUpdateMiscEffects(ILContext il) {
-			var c = new ILCursor(il);
-			var label = il.DefineLabel();
-			if (!c.TryGotoNext(MoveType.Before, i => i.MatchCall(typeof(InfernumMode.InfernumMode), "get_EmodeIsActive")))
-				throw new Exception("EMode edit failed");
-			c.MarkLabel(label);
-			if (!c.TryGotoPrev(MoveType.After, i => i.MatchCall(typeof(Main), "get_masterMode")))
-				throw new Exception("MM edit failed");
-			c.Remove();
-			c.Emit(Mono.Cecil.Cil.OpCodes.Brtrue, label);
-			if (!c.TryGotoNext(MoveType.After, i => i.MatchLdsfld(typeof(Main), "getGoodWorld")))
-				throw new Exception("FtW edit failed");
-			c.Remove();
-			c.Emit(Mono.Cecil.Cil.OpCodes.Brtrue, label);
-		}
         public override void Unload() {
             if (!ModLoader.HasMod("InfernumMode")) return;
-            HookEndpointManager.Unmodify(typeof(InfernumMode.PoDPlayer).GetMethod("PostUpdateMiscEffects", BindingFlags.Public | BindingFlags.Instance), IL_PostUpdateMiscEffects);
+			InfernumMode.Core.GlobalInstances.Systems.DifficultyManagementSystem.DisableDifficultyModes = true;
         }
     }
 }
