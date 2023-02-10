40a2f53aa9c4a43177ef9fe18a95fd328809b8b4
xeaal <tryax.use@gmail.com>
2023-02-10 19:51:24 +0500

    Fixed multiplayer support

--- InfernumMasterFtW.cs
+++ InfernumMasterFtW.cs
@@ -7,11 +7,9 @@ namespace InfernumMasterFtW
 {
 	public class InfernumMasterFtW : Mod {
 		public override void Load() {
-			if (!ModLoader.HasMod("InfernumMode")) return;
 			InfernumMode.Core.GlobalInstances.Systems.DifficultyManagementSystem.DisableDifficultyModes = false;
         }
         public override void Unload() {
-            if (!ModLoader.HasMod("InfernumMode")) return;
 			InfernumMode.Core.GlobalInstances.Systems.DifficultyManagementSystem.DisableDifficultyModes = true;
         }
     }
